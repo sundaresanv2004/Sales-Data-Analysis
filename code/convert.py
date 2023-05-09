03253c3034871056ca8bd4a08e2c6ee82ff32109
Sundaresan <117519940+sundaresanv2004@users.noreply.github.com>
2023-05-09 09:32:13 +0530

    0.01

--- code/convert.py
+++ code/convert.py
@@ -1,16 +1,39 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("Shoes.csv")
+df = pd.read_csv("Updated_Shoes.csv")
 
 list1 = []
 
 for i in range(len(df)):
-    if df.at[i, 'discount_price'] is not np.NaN:
-        list1.append(df.at[i, 'discount_price'][1:])
+    if df.at[i, 'no_of_ratings'] is not np.NaN:
+        d = df.at[i, 'no_of_ratings']
+        a = ''
+        if ',' in d:
+            for j in d:
+                a += ''.join(j) if j != ',' else ''
+            list1.append(a)
+        else:
+            list1.append(d)
     else:
-        list1.append(df.at[i, 'discount_price'])
+        list1.append(df.at[i, 'no_of_ratings'])
 
-df['discount_price'] = list1
+df["no_of_ratings"] = list1
 
 print(df.to_string())
+df.to_csv("Updated_Shoes.csv")
+
+"""
+list1 = []
+
+for i in range(len(df)):
+    if df.at[i, 'actual_price'] is not np.NaN:
+        list1.append(df.at[i, 'actual_price'][1:])
+    else:
+        list1.append(df.at[i, 'actual_price'])
+
+df['actual_price'] = list1
+
+print(df.to_string())
+df.to_csv("Updated_Shoes.cssv")
+"""
